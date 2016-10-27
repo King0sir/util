e9368c95db598a261b2c99b6ff054f58576be167
kingsir <jinminghao@ipaynow.cn>
2016-10-27 23:51:00 +0800

    getdate

--- src/main/java/org/kin/live/date/DateUtils.java
+++ src/main/java/org/kin/live/date/DateUtils.java
@@ -1,6 +1,7 @@
 package org.kin.live.date;
 
 import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
@@ -12,6 +13,14 @@ public class DateUtils {
 
     public static final String DATE_FORMAT_FULL = "yyyy-MM-dd HH:mm:ss";
 
+    public static final String DATE_FORMAT_SHORT = "yyyy-MM-dd";
+
+    public static final String DATE_FORMAT_COMPACTSHORT = "yyyyMMdd";
+
+    public static final String DATE_FORMAT_COMPACTFULL = "yyyyMMddHHmmss";
+
+    public static final String DATE_FORMAT_TIME = "HH:mm:ss";
+
     /**
      * 获取当前的时间
      * @return
@@ -21,8 +30,23 @@ public class DateUtils {
         return now.getTime();
     }
 
+    public static Date getDate(String time ,String format) throws ParseException {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(format);
+        return simpleDateFormat.parse(time);
+    }
+
     public static String getDateString(){
         DateFormat dateFormat = new SimpleDateFormat(DATE_FORMAT_FULL);
         return dateFormat.format(getDate());
+    }
+
+    public static String getDateString(String format){
+        DateFormat dateFormat = new SimpleDateFormat(format);
+        return dateFormat.format(getDate());
+    }
+
+    public static String getDateString(Date time,String format){
+        DateFormat dateFormat = new SimpleDateFormat(format);
+        return dateFormat.format(time);
     }
 }
--- src/test/java/org/kin/live/date/DateUtilTest.java
+++ src/test/java/org/kin/live/date/DateUtilTest.java
@@ -1,9 +1,23 @@
 package org.kin.live.date;
 
+import java.util.Date;
+
 /**
  * Created by kingsir on 16-10-27.
  */
 public class DateUtilTest {
+    public static void main(String[] args) {
 
+        print(DateUtils.getDate());
 
+        print(DateUtils.getDateString());
+
+        print(DateUtils.getDateString(DateUtils.DATE_FORMAT_TIME));
+
+        print(DateUtils.getDateString(new Date(),DateUtils.DATE_FORMAT_COMPACTSHORT));
+    }
+
+    private static void print(Object object){
+        System.out.println(object);
+    }
 }
